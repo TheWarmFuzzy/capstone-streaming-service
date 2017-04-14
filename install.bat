a8096c4cb9ec8251404b4ce201f831fbca252f23
Jeffrey Nelson <jt_nelson@rogers.com>
2017-04-13 20:30:32 -0400

    Lets Kill The Git Repo
    Major Restructure
    lets see how much breaks...

--- install.bat
+++ install.bat
@@ -1,9 +1,13 @@
 @echo off
+call npm install --save body-parser
 call npm install --save cookie-parser
 call npm install --save connect
 call npm install --save csurf
 call npm install --save express 
+call npm install --save express-session
+call npm install --save google-auth-library
 call npm install --save helmet
 call npm install --save multer
 call npm install --save mysql
+call npm install --save shortid
 
--- server.js
+++ server.js
@@ -1,31 +1,58 @@
 //Server Constants
 const PORT = 8080;
-const ROOT_DIRECTORY = __dirname;
-global.ROOT_DIRECTORY = ROOT_DIRECTORY;
 
 //Environment Variables
 var config = require("./configs/server.json");
-var extra_logs = require("./modules/log.js");
+var server_credentials = require("./configs/server_credentials.json");
+var extra_logs = require("./lib/log.js");
 console.log = extra_logs.date_time_log;
 
 //Server Variables
 var http = require('http');
+var https = require('https');
 var express = require('express');
+var session = require('express-session');
 var app = express();
 var server = app.listen(PORT,start);
+var path = require("path");
 
-//Directors
-var resource_director = require("./lib/resource_director.js");
-var security_director = require("./lib/security_director.js");
+//Director
+var director = require("./lib/director.js");
+
+//Engines
+var cas_engine = require("./lib/engines/cas.js");
+app.engine('cas', cas_engine);
+
+//Views
+const VIEW_WEB_DIRECTORY = path.resolve(__dirname + "/www");
+const VIEW_ERROR_DIRECTORY = path.resolve(__dirname + "/www/errors");
+const VIEW_VIDEO_DIRECTORY = path.resolve(__dirname + "/www/uploads/videos");
+
+app.set('views', [VIEW_WEB_DIRECTORY,VIEW_VIDEO_DIRECTORY,VIEW_ERROR_DIRECTORY]);
+app.set('view engine', 'cas');
+
+
+//Pre-load Session Controls
+app.use(session({
+	secret:config.session_secret,
+	resave:false,
+	saveUninitialized:false,
+	cookie:{
+		httpOnly:true,
+		maxAge:60000,
+		sameSite:false,
+		secure:false
+	}
+}));
 
 //Pre-load Security Controls
-app.use(security_director.preload());
+app.use(director.preload());
 
-//Resource Routing
-app.use("/", resource_director.www());
+//Routing
+app.use("/", director.www(app.get('views')));
 
 //Post-load Security Controls
-app.use(security_director.postload());
+app.use(director.postload());
 
 //Function that is run when the server starts
 function start()
@@ -33,3 +60,16 @@ function start()
 	//Log the server address and port
 	console.log("Starting server",server.address().address + server.address().port);
 }
+/*
+var sql = require("./lib/mysql.js");
+var procedure = {
+	"sql":"CALL cas_insert_user(@id,?,?)",
+	"values":["me@me.com","Myself"]
+};
+
+var procedure = {
+	"sql":"CALL cas_insert_video(@id,?,?,?,?,?,?,?)",
+	"values":["me@me.com","My Video","The greatest video in existance",0,0,"en",0]
+};
+console.log(sql.procedure(server_credentials.user_create,procedure,null));
+*/
